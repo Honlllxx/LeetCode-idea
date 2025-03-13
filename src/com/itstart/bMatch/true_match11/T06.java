f314a09d51562d27c809ae614dc43db3fd470582
honlllxx <2656468910@qq.com>
2025-03-13 20:32:11 +0800

    更改

--- src/com/itstart/bMatch/true_match11/T06.java
+++ src/com/itstart/bMatch/true_match11/T06.java
@@ -14,24 +14,26 @@ public class T06 {
         System.out.println("请输入数组长度:");
         int len=Integer.parseInt(sc.nextLine());
         System.out.println("请输入数组内容,空格分隔:");
-      int []nums=   Arrays.stream(sc.nextLine().split(" ")).
+      int []nums=Arrays.stream(sc.nextLine().split(" ")).
                 mapToInt(Integer::parseInt)
                 .toArray();
-      int left=0;
+
       int right=nums.length-1;
         int num=1;
-        while(left<=right){
-            if (nums[left]< nums[right]){
-                 for (int i=left+1;i<right;i++){
-                     num=nums[left]<nums[i]&&nums[i]<nums[right] ?0:1;
-                     if (num==0){
-                         count++;
-                         num=1;
+        for (int i=0;i<right;i++) {
+            int left=i;
+            while(left<=right){
+                if (nums[left]< nums[right]){
+                     for (int j=left+1;j<right;j++){
+                         num=nums[left]<nums[j]&&nums[j]<nums[right] ?0:1;
+                         if (num==0){
+                             count++;
+                             num=1;
+                         }
                      }
-                 }
-                left++;
+                }
+                right--;
             }
-            right--;
         }
         System.out.println(count);
     }
