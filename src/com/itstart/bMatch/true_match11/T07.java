bf6c628b6853181e3012e4640b6fceaf13234f53
honlllxx <2656468910@qq.com>
2025-03-13 15:58:59 +0800

    更改

--- src/com/itstart/bMatch/true_match11/T07.java
+++ src/com/itstart/bMatch/true_match11/T07.java
@@ -6,23 +6,50 @@ import java.util.Scanner;
 
 public class T07 {
     public static void main(String[] args) {
-        getFourWord();
-
+        System.out.println(getFourWord());
     }
     public static String getFourWord(){
         Scanner sc=new Scanner(System.in);
-        System.out.println("输入单词");
-        String input=sc.nextLine();
-        char []ch=input.toCharArray();
+        System.out.println("请输入单词");
+        String word=sc.nextLine();
 
+        int index=0;
+        int len=word.length();
+        boolean hasConsonat1=false;
+        while (index<len&&!isVowel(word.charAt(index))){
+            hasConsonat1=true;
+            index++;
+        }
+        if (!hasConsonat1){
+            return "no";
+        }
 
+        boolean hasVowel1=false;
+        while (index<len&&isVowel(word.charAt(index))){
+            hasVowel1=true;
+            index++;
+        }
 
+        if (!hasVowel1){
+            return "no";
+        }
+        boolean hasConsonat2=false;
+        while (index<len&&!isVowel(word.charAt(index))){
+            hasConsonat2=true;
+            index++;
+        }
 
-
-
-
-
-return null;
-
+        boolean hasVowel2=false;
+        while (index<len&&isVowel(word.charAt(index))){
+            hasVowel2=true;
+            index++;
+        }
+        if (!hasVowel2){
+            return "no";
+        }
+        return "yes";
+    }
+    public static boolean isVowel(char ch){
+        return ch=='a'||ch=='e'||ch=='o'||ch=='i'||ch=='u';
     }
 }
