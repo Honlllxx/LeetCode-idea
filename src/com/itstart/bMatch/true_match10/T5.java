f0fd73118ae3de5f8664e879d19d269fe5e2fcea
honlllxx <2656468910@qq.com>
2025-03-18 14:41:03 +0800

    test1

--- src/com/itstart/bMatch/true_match10/T5.java
+++ src/com/itstart/bMatch/true_match10/T5.java
@@ -2,6 +2,6 @@ package com.itstart.bMatch.true_match10;
 
 public class T5 {
     public static void main(String[] args) {
-        System.out.println("Hello!!!!!!!!");
+//Test!~~~~~~~~~~~
     }
 }
